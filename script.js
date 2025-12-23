4bb98e459a2bfdf9c83e6a14152322b8c02eea72
DenvWest <DenvWest@users.noreply.github.com>
2025-12-23 16:34:55 +0100

    Fix JS file and verify console output

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 console.log("hello world");
 
+function getComputerChoice() {
+  const r = Math.random();
+  if (r < 1 / 3) return "rock";
+  if (r < 2 / 3) return "paper";
+  return "scissors";
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
